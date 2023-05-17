f2bfe09a5f666d68dee50b174ead4721b98013ab
0xBigBee <0xbigbee@protonmail.com>
2023-05-17 08:33:08 -0700

    feat: adding cors headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,32 @@
 module.exports = {
 	reactStrictMode: true,
 	eslint: {
-		dirs: ['pages', 'components', 'utils', 'config'],
+		dirs: ["pages", "components", "utils", "config"],
 	},
 	experimental: {
 		outputStandalone: true,
-	}
+	},
+	async headers() {
+		return [
+			{
+				// matching all processing js files
+				source: "/lib/processing/*",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "https://openprocessing.org",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value:
+							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+					},
+				],
+			},
+		];
+	},
 };
